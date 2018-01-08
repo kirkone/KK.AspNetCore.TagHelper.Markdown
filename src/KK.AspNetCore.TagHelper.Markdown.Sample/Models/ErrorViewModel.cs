ad3abe7421bd8177713bc95420affd8def708e08
Kirsten Kluge <kirsten.kluge@t-systems.com>
2018-01-08 09:17:24 +0100

    initial commit

---
+++ src/KK.AspNetCore.TagHelper.Markdown.Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KK.AspNetCore.TagHelper.Markdown.Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ src/KK.AspNetCore.TagHelper.Markdown/MarkdownTagHelper.cs
@@ -0,0 +1,49 @@
+﻿namespace KK.AspNetCore.TagHelper.Markdown
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+    [HtmlTargetElement("markdown")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        [HtmlAttributeName("text")]
+        public string Text { get; set; }
+
+        [HtmlAttributeName("surrounding-tag")]
+        public string SurroundingTag { get; set; }
+
+        [HtmlAttributeName("surrounding-class")]
+        public string SurroundingClass { get; set; }
+
+        [HtmlAttributeName("source")]
+        public ModelExpression Source { get; set; }
+
+        public override void Process(
+            TagHelperContext context,
+            TagHelperOutput output
+        )
+        {
+            if (this.Source != null)
+            {
+                this.Text = this.Source.Model.ToString();
+            }
+
+            var result = Markdig.Markdown.ToHtml(this.Text);
+
+            if (!string.IsNullOrWhiteSpace(SurroundingTag))
+            {
+                output.TagName = SurroundingTag;
+                if (!string.IsNullOrWhiteSpace(SurroundingClass))
+                {
+                    output.Attributes.Add("class", SurroundingClass);
+                }
+                output.TagMode = TagMode.StartTagAndEndTag;
+            }
+            else
+            {
+                output.TagName = null;
+            }
+            output.Content.SetHtmlContent(result);
+        }
+    }
+}
